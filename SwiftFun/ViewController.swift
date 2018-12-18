2419ba464939808f6f32e96e4dedd39571cf5f81
Michael Kozub <michaelkozub@gmail.com>
2018-12-18 10:04:48 -0700

    Initial Commit

---
+++ SwiftFun/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwiftFun
+//
+//  Created by Michael Kozub on 12/18/18.
+//  Copyright © 2018 Michael Kozub. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
