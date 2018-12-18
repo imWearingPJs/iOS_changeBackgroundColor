d2ae1a603b1b6ec2bb9e6e76e2c1aa57271fe86c
Michael Kozub <michaelkozub@gmail.com>
2018-12-18 15:27:30 -0700

    stable, working

--- SwiftFun/ViewController.swift
+++ SwiftFun/ViewController.swift
@@ -10,11 +10,33 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var testLabel: UILabel!
+    
+    var count = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        //view.backgroundColor = UIColor.red
+    }
+    
+    func getRandomColor() -> UIColor {
+        //Generate between 0 to 1
+        let red:CGFloat = CGFloat(drand48())
+        let green:CGFloat = CGFloat(drand48())
+        let blue:CGFloat = CGFloat(drand48())
+        
+        return UIColor(red:red, green: green, blue: blue, alpha: 1.0)
     }
 
-
+    @IBAction func buttonTap(_ sender: Any) {
+        print("Hey the button was tapped :)")
+        count = count + 1
+        testLabel.text = String(count)
+        if (count >= 10){
+            view.backgroundColor = getRandomColor()
+        }
+    }
+    
 }
 
